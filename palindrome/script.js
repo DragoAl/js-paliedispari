778b2d4c61f6f7903f8bb33ed1270bbd1854b6f2
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-10-29 17:11:32 +0200

    1 es

---
+++ palindrome/script.js
@@ -0,0 +1,41 @@
+// PALINDROME Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+// Dare l’output relativo.
+
+const wordCont = document.getElementById("word");
+// CHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA
+
+ 
+let userWord = prompt("Inserisci una parola");
+
+functPalindrome(userWord);
+
+
+// INVERTO LA PAROLA INSERITA
+// const reverseWord = userWord.split("").reverse().join("");
+// console.log(reverseWord);
+
+// CONFRONTO SE LA PAROLA INSERITA E' UGUALE ALLA PAROLA INVERTITA
+// if(userWord === reverseWord){
+//     console.log("è una parola palindroma");
+// }else{
+//     console.log("non è palindroma");
+// }
+
+
+// FUNZIONE 
+function functPalindrome(inputWord) {
+    // INVERTO LA PAROLA INSERITA
+    let reverseWord = inputWord.split("").reverse().join("");
+    console.log(reverseWord);
+
+    // CONFRONTO LA PAROLA INVERTITA CON QUELLA INSERITA
+    if(inputWord === reverseWord){
+        console.log("è una parola palindroma");
+       
+    }else{
+        console.log("non è palindroma");
+    }
+
+}
+
